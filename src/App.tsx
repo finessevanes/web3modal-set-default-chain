5d78685321f1ef5c5ba42be07395616699abb6bd
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2023-05-03 17:58:34 -0500

    add project ID

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,34 @@
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/react'
-import { configureChains, createClient, WagmiConfig } from 'wagmi'
-import { arbitrum, mainnet, polygon } from 'wagmi/chains'
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+import { Web3Button } from "@web3modal/react";
 
-const chains = [arbitrum, mainnet, polygon]
-const projectId = 'YOUR_PROJECT_ID'
+const chains = [arbitrum, mainnet, polygon];
+const projectId = import.meta.env.VITE_PROJECT_ID;
 
-const { provider } = configureChains(chains, [w3mProvider({ projectId })])
+const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
 const wagmiClient = createClient({
   autoConnect: true,
   connectors: w3mConnectors({ projectId, version: 1, chains }),
-  provider
-})
-const ethereumClient = new EthereumClient(wagmiClient, chains)
+  provider,
+});
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 function App() {
   return (
     <>
       <WagmiConfig client={wagmiClient}>
-        <h1>Test</h1>
+        <Web3Button />
       </WagmiConfig>
 
       <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
